a4d8a4bacb2d283ccc57ced549ee909b9e29ef6c
Rakshit <rakshit.nawani@ushyaku.com>
2018-06-11 16:54:29 +0530

    Initial commit
    Initial Commit

---
+++ signaturecapture/src/main/java/com/rakshit/signaturecapture/SignatureVIew.java
@@ -0,0 +1,13 @@
+package com.rakshit.signaturecapture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignatureVIew extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signature_view);
+    }
+}
